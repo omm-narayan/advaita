185624be79e52c2084903c65b798648909910d9c
omm-narayan <omm.codes@gmail.com>
2025-11-03 00:12:34 +0530

    update

---
+++ src/data/sequence_loader.py
@@ -0,0 +1,24 @@
+import torch
+from torch.utils.data import Dataset
+
+class SequenceDataset(Dataset):
+    """Load and process DNA sequences for CRISPR guides"""
+    
+    def __init__(self, fasta_file, guide_positions, labels):
+        self.genome = self._load_genome(fasta_file)
+        self.guide_positions = guide_positions
+        self.labels = labels
+        
+    def __len__(self):
+        return len(self.guide_positions)
+    
+    def __getitem__(self, idx):
+        # TODO: Implement sequence extraction
+        sequence = self._extract_sequence(self.guide_positions[idx])
+        label = self.labels[idx]
+        return sequence, label
+        
+    def _load_genome(self, fasta_file):
+        """Load reference genome"""
+        # Implementation here
+        pass
